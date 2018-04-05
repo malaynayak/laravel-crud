f96d4404e38a3e02ecff5694c2636270e7d896fb
Malay Nayak <malay.kanta.nayak@gmail.com>
2018-04-05 17:25:33 +0530

    Passed variable to layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        view()->composer('layouts.main', function($view) {
+            $categories = Category::get();
+            $view->with('categories', $categories);
+        });
     }
 
     /**
