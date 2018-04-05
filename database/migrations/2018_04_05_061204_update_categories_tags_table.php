af036cdff5e6f033a784b1ab853542edb808ab84
Malay Nayak <malay.kanta.nayak@gmail.com>
2018-04-05 14:22:56 +0530

    DB Updates

--- database/migrations/2018_04_05_061204_update_categories_tags_table.php
+++ database/migrations/2018_04_05_061204_update_categories_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class UpdateCategoriesTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
+        Schema::table('tags', function (Blueprint $table) {
+            $table->string('name')->change();
+        });
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('title')->change();
         });
     }
 
@@ -26,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        //
     }
 }
